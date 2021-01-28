40a870c8ae52340da19842ef209044220b110c6d
umaibou1126 <umaibou1126@gmail.com>
2021-01-28 11:13:20 +0900

    20210128

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/umaibou1126/goblockchain
 
 go 1.14
+
+require (
+	github.com/btcsuite/btcutil v1.0.2
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+)
--- main.go
+++ main.go
@@ -15,6 +15,10 @@ func main() {
 	w := wallet.NewWallet()
 	fmt.Println(w.PrivateKeyStr())
 	fmt.Println(w.PublicKeyStr())
+	fmt.Println(w.BlockchainAddress())
+
+	t := wallet.NewTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
+	fmt.Printf("signature %s\n", t.GenerateSignature())
 	// myBlockchainAddress := "my_blockchain_address"
 	// blockChain := NewBlockchain(myBlockchainAddress)
 	// blockChain.Print()
